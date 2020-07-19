6003df204daafda79e973bf86f10f6d814625be2
Morgan <morgan.moreno25@gmail.com>
2020-07-19 18:27:40 -0500

    included FormModule for ngModule
    included HttpClientModule for services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WEATHER_API_KEY } from './ENV';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather-app';
+  title = 'Weather App';
 }
