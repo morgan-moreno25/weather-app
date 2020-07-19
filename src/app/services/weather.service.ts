a7fea9b55d9d3875c9dd1482f3d5031e208d0a05
Morgan <morgan.moreno25@gmail.com>
2020-07-19 18:31:17 -0500

    Gets weather data from open weather

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WEATHER_API_KEY } from '../ENV';
+import { Weather, WeatherResponse } from '../Types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  weatherData: Object;
+
+  weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+  getWeather(city:String): Observable<Object> {
+    let data = this.http.get(this.weatherURL + city + `&appid=${WEATHER_API_KEY}`);
+    return data;
+  }
+  
+}
